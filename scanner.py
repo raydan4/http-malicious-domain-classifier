231dc9681b442e1bbd7a4d81540db9017f3ddbf3
Hulto <jack.m.mckenna@gmail.com>
2019-11-21 17:03:06 -0500

    NOt sure what's going on with requests

--- scanner.py
+++ scanner.py
@@ -1,16 +1,41 @@
 """
 Read domain list and export data to file
 """
-import requests as r
+import requests
 from bs4 import BeautifulSoup
 from argparse import ArgumentParser
 from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+
+
+from sqlalchemy import Column, Integer, String, BLOB, Date, ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 parser = ArgumentParser()
 parser.add_argument('infile', help='File to read url from')
 args = parser.parse_args()
 
 # TODO: figure out how to convert to integer consistently, or don't. Maybe same as tags
 # Read mimes and create mime dict
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:////tmp/tmpdb.sql')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Record(Base):
+    __tablename__ = 'record'
+
+    id = Column(Integer, primary_key=True)
+    redirect_count = Column(Integer)
+    html_vector = Column(String)
+    mime_type = Column(String)
+    url_entropy = Column(String)
+    links_on_page = Column(String)
 
 # Read domains from file
 with open(args.infile, 'r') as f:
@@ -50,6 +75,9 @@ def analyze_url(url):
     assemble feature data for url
     """
     # Feature list: url entropy, number of redirects, html skeleton vector
+    tmp = url[0]
+    if "http" not in tmp:
+        tmp = "http://" + tmp
     r = requests.get(url[0])
 
     # If request failed, return False
@@ -69,26 +97,16 @@ def analyze_url(url):
     html_skeleton_vector = vector
     links_on_page = links
     
-    # Export data to database
-    # TODO: do the sqlalchemy
-
+    myrecord = Record(redirect_count=number_of_redirects, \
+                html_vector=html_skeleton_vector, \
+                mime_type=mime_type, \
+                url_entropy=url_entropy, \
+                links_on_page=links_on_page)
+    session.add(myrecord)
+    session.commit()
     return True
 
-#with concurrent.futures.ThreadPoolExecutor() as executor:
-#    # Make requests to all domains
-#    for url, result in zip(domains, executor.map(analyze_url, domains))
-#        print(f'{url[0]} scanned and added: {result}')
-debug = True
-if __name__ == "__main__":
-    if( debug == True ):
-        """
-        # Test analyze_html
-        test_html = ""
-        with open('testhtml.html', 'r') as f:
-            line = f.readline()
-            while(line):
-                test_html += line
-                line = f.readline()
-
-        print(analyze_html(test_html))
-        """
+with ThreadPoolExecutor() as executor:
+    # Make requests to all domains
+    for url, result in zip(domains, executor.map(analyze_url, domains)):
+        print(f'{url[0]} scanned and added: {result}')
